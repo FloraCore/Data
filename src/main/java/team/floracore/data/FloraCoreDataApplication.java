11cf7a9a9a0b5d28d4e4069142b4098d8b41d84f
xLikeWATCHDOG <bilibili.mer@outlook.com>
2023-06-02 12:14:40 +0800

    init

---
+++ src/main/java/team/floracore/data/FloraCoreDataApplication.java
@@ -0,0 +1,13 @@
+package team.floracore.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloraCoreDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FloraCoreDataApplication.class, args);
+    }
+
+}
